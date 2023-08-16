f031d4ffdf53e3453ee84c768ab7ba8a5f26b20a
Ben Moulton <ben.moulton1@icloud.com>
2023-08-16 23:35:56 +0100

    Added Necessary Game Manager Variables, and made them inspector friendly

--- Game_Jam_Project/Assets/Scripts/Game_Controller.cs
+++ Game_Jam_Project/Assets/Scripts/Game_Controller.cs
@@ -4,11 +4,80 @@ using UnityEngine;
 
 public class Game_Controller : MonoBehaviour
 {
-    #region 
+
+    [SerializeField] GameObject gameManager;
+
+    #region Player Movement
+    [Space]
+    [Header("Player Movement")]
+    public float playerMovementSpeed;
+    [Space]
+    #endregion
+
+    #region Player Attacks
+    [Header("Player Attack")]
+    public string[] playerWeapons;
+    public int maxPlayerAttackCombo;
+    [Space]
+    #endregion
+
+    #region Player Health
+    [Header("Player Health")]
+    public int maxPlayerHealth;
+    public int currentPlayerHealth;
+    [Space]
+    #endregion
+
+    #region Terrain Management
+    [Header("Terrain Management")]
+    [Space]
+    #endregion
+
+    #region Mob Movement
+    [Header("Mob Movement")]
+    public float mob1MovementSpeed;
+    public float mob2MovementSpeed;
+    public float mob3MovementSpeed;
+    public float mob4MovementSpeed;
+    public float mob5MovementSpeed;
+    public float mob6MovementSpeed;
+    [Space]
+    #endregion
+
+    #region Mob Attacks
+    [Header("Mob Attacks")]
+    public int mob1AttackDamage;
+    public int mob2AttackDamage;
+    public int mob3AttackDamage;
+    public int mob4AttackDamage;
+    public int mob5AttackDamage;
+    public int mob6AttackDamage;
+    [Space]
+    #endregion
+
+    #region Mob Health
+    [Header("Mob Health")]
+    public int mob1MaxHealth;
+    public int mob2MaxHealth;
+    public int mob3MaxHealth;
+    public int mob4MaxHealth;
+    public int mob5MaxHealth;
+    public int mob6MaxHealth;
+    [Space]
+    #endregion
+
+    #region Animator
+    [Header("Animator")]
+    [SerializeField] public GameObject animator;
+    #endregion
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(gameManager);
+        DontDestroyOnLoad(animator);
     }
 
     // Update is called once per frame
